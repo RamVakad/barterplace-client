7d9ad3ed64ab697f605aedb368dd84e883c99c15
Cristopher Jimenez <crisjimenez120@gmail.com>
2019-04-02 22:10:06 -0400

    added routes

--- barterplace/src/App.js
+++ barterplace/src/App.js
@@ -1,25 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { Route, Switch } from "react-router-dom";
+import Home from "./Components/Home/Home";
+import Login from "./Components/Login/Login";
+import Register from "./Components/Register/Register";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/Login" component={Login} />
+          <Route path="/Register" component={Register} />
+        </Switch>
       </div>
     );
   }
