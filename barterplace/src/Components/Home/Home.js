895a0cfb709b663514bfc1f666699eab3a562a7c
Rene Sanger <renesanger@Renes-MacBook-Pro.local>
2019-04-15 01:24:59 -0400

    CSS STYLING

--- barterplace/src/Components/Home/Home.js
+++ barterplace/src/Components/Home/Home.js
@@ -6,10 +6,14 @@ import { Redirect } from "react-router-dom";
 import Navigation from "../Navigation/Navigation";
 import AddItem from "../AddItem/AddItem";
 import ItemList from "../ItemList/ItemList";
-import SearchBox from "../SearchBar/SearchBar";
 import { Button } from "@material-ui/core";
 
+import "./bootstrap.css"
+import "./bootstrap.min.css"
+import styles from "./bootstrap.css"
+import logo from  "./hunter-college-logo.png"
 import "./Home.css";
+import Navbar from "../Navbar/Navbar";
 
 class Home extends Component {
   constructor(props) {
@@ -48,23 +52,61 @@ class Home extends Component {
     return (
       <div className="Home">
         <Navigation signOut={this.signOut} />
-        <h1>Home</h1>
-        <SearchBox />
-        <Button
-          variant="contained"
-          size="large"
-          color="primary"
-          onClick={this.toggleAddItemModal}
-        >
-          Add Item
-        </Button>
+        <img src={require("./hunter-college-logo.png")} width="20%"/>
+        <h1 id="banner">Hunter Barter App</h1>
+        
         <Modal open={this.state.addItemModal} onClose={this.toggleAddItemModal}>
           <DialogContent>
             <AddItem />
           </DialogContent>
         </Modal>
-        <ItemList />
+        
+        <Navbar addItem={this.toggleAddItemModal}></Navbar>
+
+        
+        <div id="body"> 
+          
+        <div  id="card-wrapper" class="card  mb-3" >
+          <div class="card-body">
+          <ItemList id="item"/>
+            <div class="card bg-light mb-3" >
+            <div class="card-header">Computers and Technology</div>
+              <div class="card-body">
+                <img src={require("./macbook.jpg")}></img>
+                <h4 class="card-title">Macbook Pro 13-inch retina display Quad Core</h4>
+                <p class="card-text">Good for Editing videos, Photoshop, Adobe, and Coding with Visual Studios. 256 GB Storage with 16GB RAM Customized laptop</p>
+                <p>Condition: NEW</p>
+                <a>Contact Seller</a>
+              </div> 
+            </div>
+
+            <div class="card bg-light mb-3" >
+            <div class="card-header">Things that are Fire</div>
+              <div class="card-body">
+                <img src={require("./mixtape.jpeg")}></img>
+                <h4 class="card-title">Rene's Super Fire Mixtape</h4>
+                <p class="card-text">Proceed with Caution.</p>
+                <p>Condition: GOOD</p>
+                <a>Contact Seller</a>
+              </div> 
+            </div>
+
+            <div class="card bg-light mb-3" >
+            <div class="card-header">Eduction and Books</div>
+              <div class="card-body">
+                <img src={require("./textbook.jpg")}></img>
+                <h4 class="card-title">Economics Textbook</h4>
+                <p class="card-text">Aggregate Demand and Aggregate Supply Graphs and the History of Economics.</p>
+                <p>Condition: GOOD</p>
+                <a>Contact Seller</a>
+              </div> 
+            </div>
+          </div>
+        </div>
+        </div>
+
       </div>
+      
     );
   }
 }
--- barterplace/src/Components/SearchBar/SearchBar.js
+++ barterplace/src/Components/SearchBar/SearchBar.js
@@ -9,7 +9,10 @@ class SeachBar extends Component {
   render() {
     return (
       <div className="SeachBar">
-        <h1>SeachBar</h1>
+        <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="text" placeholder="Search"/>
+            <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+          </form>
       </div>
     );
   }
