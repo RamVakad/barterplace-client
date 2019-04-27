f6bf26333711829adb3391440a3033fddfcd6483
bobur <uzxonboy@hotmail.com>
2019-04-27 11:37:55 -0400

    fixed indentation errors

--- barterplace/src/Components/Navigation/Navigation.js
+++ barterplace/src/Components/Navigation/Navigation.js
@@ -4,17 +4,17 @@ import { Button } from "@material-ui/core";
 const Navigation = ({ signOut }) => {
   return (
     <nav style={{ display: "flex", justifyContent: "flex-left" }}>	  
-		<Button
-			variant="contained"
-            size="large"
-            color="primary"
-            onClick={() => {
-				signOut();
-				sessionStorage.clear();
-            }}
-        >
-        Sign Out
-        </Button>
+     <Button
+       variant="contained"
+       size="large"
+       color="primary"
+       onClick={() => {
+         signOut();
+		 sessionStorage.clear();
+       }}
+     >
+     Sign Out
+     </Button>
     </nav>
   );
 };
